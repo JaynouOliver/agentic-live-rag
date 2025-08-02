5843e3e455753cca42f99daf8026b99050e51292
Suvrakamal Das <jyb2084.usa@gmail.com>
2025-08-02 16:41:22 +0530

    Add initial project files for vector search app
    Added main.py for loading and querying blog posts using LangChain and OpenAI embeddings, vectorDB.py for Pinecone vector database setup and sample data, requirements.txt for dependencies, and .env for API keys. This sets up the foundation for a vector search application integrating document retrieval and LLM-based responses.

---
+++ main.py
@@ -0,0 +1,79 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.graph import MessagesState
+from langchain.chat_models import init_chat_model
+
+
+
+
+load_dotenv()  
+
+# Set a user agent to identify your requests (Mac-specific)
+os.environ["USER_AGENT"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+def _set_env(key: str):
+    if key not in os.environ:
+        os.environ[key] = getpass.getpass(f"{key}:")
+
+
+_set_env("OPENAI_API_KEY")
+
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+doc_splits[0].page_content.strip()
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts.",
+)
+retriever_tool.invoke({"query": "types of reward hacking"})
+
+response_model = init_chat_model("openai:gpt-4.1", temperature=0)
+
+
+def generate_query_or_respond(state: MessagesState):
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = (
+        response_model
+        .bind_tools([retriever_tool]).invoke(state["messages"])
+    )
+    return {"messages": [response]}
+
+input = {
+    "messages": [
+        {
+            "role": "user",
+            "content": "What does Lilian Weng say about types of reward hacking?",
+        }
+    ]
+}
+
+generate_query_or_respond(input)["messages"][-1].pretty_print()
